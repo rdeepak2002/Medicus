32d4ac4e2c944412f263c878dbb5768e081c2921
Deepak Ramalingam <rdeepak2002@gmail.com>
2019-04-20 15:27:55 -0700

    added home page

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express')
+const path = require('path');
+
 var app = express();
 
 var port = process.env.PORT || 8080;
@@ -7,6 +9,10 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
 	res.render('index');
+})
+
+app.get('/home', function(req, res) {
+	res.sendFile(path.join(__dirname+'/public/pages/home.html'));
 })
 
 app.listen(port, function() {
