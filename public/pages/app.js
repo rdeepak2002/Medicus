0bcf06eba5b810fe88f791867a6eefa2ec55abe6
Deepak Ramalingam <rdeepak2002@gmail.com>
2019-04-20 17:53:03 -0700

    have to fix error when deployed on web server

--- public/pages/app.js
+++ public/pages/app.js
@@ -1,4 +1,10 @@
 $(function() {												// wait for document to be ready
+	getDisease();
+});
+
+function getDisease() {
+	console.log("making http request");
+
 	$.get("/getDisease", function(data, status) {			// make http request to getDisease
     	if(status == "success") {
     		$("#output").text(data);
@@ -7,4 +13,4 @@ $(function() {												// wait for document to be ready
     		alert("Error making http request to server.");
     	}
 	});
-});
+}
