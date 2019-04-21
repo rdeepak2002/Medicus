884f689c80390dde102cf3e05118412ce45c7a7b
Deepak Ramalingam <rdeepak2002@gmail.com>
2019-04-21 12:19:37 -0700

    downloads file instead

--- server.js
+++ server.js
@@ -18,26 +18,22 @@ app.get('/home', function(req, res) {								// returns home page
 })
 
 app.get('/getDisease', function(req, res) {							// function to get disease and sending it back using http response
+
 	var exec = require('child_process').exec;
 	var command = 'wolframscript Predictor.wls';
 	var child;
 
-	exec("pwd", (error, stdout, stderr) => {
-		console.log(stdout);
+	child = exec(command, function (getErr, out, err) {
+  		console.log('output: ' + out);
+  		console.log('error: ' + err);
 
-		command += " " + stdout;
+  		res.send(out);
 
-		child = exec(command, function (err1, out, err) {
-	  		console.log('output: ' + out);
-	  		console.log('error: ' + err);
+      	if (getErr !== null) {
+        	console.log('exec error: ' + getErr);
+      	}
+    });
 
-	  		res.send(out);
-
-	      	if (err1 !== null) {
-	        	console.log('exec error: ' + err1);
-	      	}
-	    });
-	})
 })
 
 
